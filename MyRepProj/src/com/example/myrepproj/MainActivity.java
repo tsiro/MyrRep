a0a55b8165716e1f5c76ff16b9bc9f964aa6f628
giannis <giannis@giannis-PC>
2014-05-14 12:22:09 +0300

    variable declaration (second commit)

--- MyRepProj/src/com/example/myrepproj/MainActivity.java
+++ MyRepProj/src/com/example/myrepproj/MainActivity.java
@@ -13,6 +13,8 @@ import android.os.Build;
 
 public class MainActivity extends ActionBarActivity {
 
+	int x;
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
